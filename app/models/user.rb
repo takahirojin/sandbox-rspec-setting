27ca73138cc1d2ade9e370484e6127fa8cf4a329
takahirojin <takahiro.jinno@gmail.com>
2022-01-11 14:08:50 +0900

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   class << self
     def find
-      { id: 1, first_name: 'first-111', last_name: 'last-111' }
+      { id: 1, first_name: 'first-111', last_name: 'last-111', full_name: '' }
     end
   end
 end
