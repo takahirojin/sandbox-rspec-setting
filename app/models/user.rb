cf6c05120071be19a6729dc4930af5c8840c37c4
takahirojin <takahiro.jinno@gmail.com>
2022-01-11 14:43:41 +0900

    Update dosen't pass spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   class << self
     def find
-      { id: 1, first_name: 'first-111', last_name: 'last-111', full_name: '' }
+      { id: 1, first_name: 'first-111', last_name: 'last-111', full_name: 'first-111 last-111' }
     end
   end
 end
